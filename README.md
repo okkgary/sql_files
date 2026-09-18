7e7092d686550e69ffe443b5d051f6a955825634
Gaurav Singh <gaurav.hrishibusiness@gmail.com>
2026-09-19 00:19:53 +0530

    update_top_jobs

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-hello there
+# INTRODUCTION
+Dive into the data job market! Focusing on data analyst roles, this project explores top paying jobs, in-demand skills, and where high demand meets salary in data analytics.
+Searching SQL Queries ! Check them out here : [project_sql folder](/project_sql/)
+# TOOLS I USED
+For my deep dive into the data analyst job market, I harnessed the power of several key tools :
+ - **SQL :** The backbone of my analysis, allowing me to query the database and unearth critical insights.
+ - **PostgresSQL :** The chosen database management system, ideal for handling the job posting data.
+ - **Visual Studios Code :** My go-to for database management and executing SQL queries.
+ - **Git & Github :** Essential for version control and sharing my SQL scripts and analysis, ensuring colloboration and project tracking.
+# THE ANALYSIS
+# WHAT I LEARNED
+# CONCLUSION
--- project_sql/top_jobs.sql
+++ project_sql/top_jobs.sql
@@ -1,4 +1,4 @@
-WITH table_1 AS (
+with table_1 as (
             
 SELECT
     job_postings_fact.job_id AS ID,
@@ -9,17 +9,14 @@ FROM job_postings_fact
 LEFT JOIN company_dim
     ON job_postings_fact.company_id = company_dim.company_id
 WHERE
-    job_postings_fact.job_work_from_home = TRUE AND
     job_postings_fact.salary_year_avg IS NOT NULL AND
     job_postings_fact.job_title_short = 'Data Analyst' 
-ORDER BY job_postings_fact.salary_year_avg DESC
+
 )
 SELECT 
-    --POSITION,
-    --COMPANY,
     round(avg(SALARY),0) AS average_salary,
     skills_dim.skills AS SKILLS,
-    count(*) AS demand_count
+    COUNT(distinct table_1.ID) AS demand_count
 FROM table_1
 INNER JOIN skills_job_dim
     ON table_1.ID = skills_job_dim.job_id
@@ -27,9 +24,7 @@ INNER JOIN skills_dim
     ON skills_job_dim.skill_id = skills_dim.skill_id
 GROUP BY
     skills
-HAVING
-    count(*) > 10
-ORDER BY 
-    average_salary desc, 
-    demand_count desc
-LIMIT 10 ;
+having
+    count(distinct table_1.ID) > 10
+Order by average_salary desc , demand_count desc
+limit 10
