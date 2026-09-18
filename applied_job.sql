e5fb5e5a44e7288222bd5076abec28dba6bb3a3c
Gaurav Singh <gaurav.hrishibusiness@gmail.com>
2026-09-18 12:25:44 +0530

    push example

---
+++ applied_job.sql
@@ -0,0 +1,13 @@
+ALTER TABLE my_table
+RENAME COLUMN app_date TO application_date;
+
+
+
+alter table my_table
+alter column name type text;
+
+alter table my_table
+drop column application_date;
+
+select *
+from my_table;
